6695c20f8798e79ece0ca4dc14236b8cd87d5577
nicokrym <167230566+nicokrym@users.noreply.github.com>
2025-05-12 11:43:54 -0300

    Funcion clonar creada

---
+++ Assets/Scripts/INSTANTIATOR.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class INSTANTIATOR : MonoBehaviour
+{
+    public GameObject cubePreFab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clonarCubo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clonarCubo()
+    {
+        Instantiate(cubePreFab);
+    }
+}
